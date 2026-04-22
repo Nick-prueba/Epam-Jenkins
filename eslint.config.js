b6f3b22ab41ba6be78c57ede613a7b9785d1ee47
Victoria Inc <Victoriaincinga@gmail.com>
2026-04-22 17:29:48 -0300

    last configs

--- eslint.config.js
+++ eslint.config.js
@@ -3,10 +3,17 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  {
-    files: ["**/*.{js,mjs,cjs}"],
-    plugins: { js },
-    extends: ["js/recommended"],
-    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+  { 
+    files: ["**/*.{js,mjs,cjs}"], 
+    plugins: { js }, 
+    extends: ["js/recommended"], 
+    languageOptions: { globals: {...globals.browser, ...globals.node} },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-compare-neg-zero": "warn",
+      "no-debugger": "off",
+      "no-unused-private-class-members": "warn",
+      "valid-typeof": "warn"
+    }
   },
 ]);
